543dbbeb73d22fc2d6f661e445a4a2b321f2c9a5
hed1ad <diad.hedi@gmail.com>
2025-12-24 18:37:57 +0300

    log

--- main.go
+++ main.go
@@ -3,7 +3,19 @@ package main
 import (
 	"hed1ad/llm_golang/llm"
 	"hed1ad/llm_golang/scripts"
+	"os"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	if os.Getenv("DEBUG") != "" {
+		log.SetLevel(log.DebugLevel)
+		log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	} else {
+		log.SetLevel(log.InfoLevel)
+	}
+}
 
 func main() {
 	resp, err := llm.CallLLM("Напиши скрипт на python который выводит hello world!")
