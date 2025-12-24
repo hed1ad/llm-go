0e88db0681f5499ab266a5160fb017c76959dad6
hed1ad <diad.hedi@gmail.com>
2025-12-24 18:57:57 +0300

    add debug logs

--- scripts/writescript.go
+++ scripts/writescript.go
@@ -3,10 +3,13 @@ package scripts
 import (
 	"fmt"
 	"os"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func CreateScript(script string) {
 
+	log.Debug("Создаем файл скрипта")
 	file, err := os.Create("script.py")
 
 	if err != nil {
